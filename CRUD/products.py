4538cd1ae9cdaf09497efe5f461faa9b86df1540
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-05-13 10:22:51 -0400

    Added Routes

--- CRUD/products.py
+++ CRUD/products.py
@@ -11,8 +11,46 @@ products = [
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
 
-#
-#Add all the REST API end-points here
-#
+
+# Request Product list
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+
+# Get a spcific product
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    return jsonify(product)
+
+
+# Add product to list
+@app.route('/products', methods=['POST'])
+def add_products():
+    products.append(request.get_json())
+    return '', 201
+
+
+# Update product information
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x["id"] == id][0]
+    for key, value in updated_product.items():
+        product[key] = value
+    return '', 204
+
+
+# Delete product
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    products.remove(product)
+    return '', 204
+
 
 app.run(port=5000,debug=True)
